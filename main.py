b590c9a9f8a6b4a0f85170e04e7120eabd8156f0
HoDzha <hodzha@gmail.com>
2024-08-23 21:41:12 +0500

    Накодил бой

--- main.py
+++ main.py
@@ -19,19 +19,19 @@
 from abc import ABC, abstractmethod
 
 # боец
-Class Fighter()
-    def __init__(self,name,power):
+class Fighter:
+    def __init__(self, name, power):
         self.power = power
         self.name = name
-        self.weapon = None
-# монстр
-Class Monster()
-    def __init__(self, health):
+        self.weapon = None  # Default weapon set to None
+
+class Monster:
+    def __init__(self, name, health):
+        self.name = name
         self.health = health
 
 # Абстраткный класс для оружия
-Class Weapon(ABC)
-
+class Weapon(ABC):
     @abstractmethod
     def __init__(self):
         pass
@@ -39,41 +39,66 @@ Class Weapon(ABC)
     def attack(self):
         pass
 
-Class Bow(Weapon) #лук
+class Bow(Weapon):
     def __init__(self, name, range, damage):
         self.name = name
         self.range = range
         self.damage = damage
-    def attack(self,monster):
-        monster.health -= self.damage
+    def attack(self, monster,power):
+        monster.health -= self.damage*power
         if monster.health <= 0:
-            print(f"Победа! Монстр убит оружием {self.name} c расстоянием {self.range}!")
+            print(f"Победа! {monster.name} убит оружием {self.name} c расстоянием {self.range}!")
         else:
-            print(f"Боец наносит удар! Монстр получил {self.damage} урона оружием {self.name} с расстояния {self.range} и осталось {monster.health} здоровья.")
+            print(f"Боец наносит удар! {monster.name} получил {self.damage*power} урона оружием {self.name} с расстояния {self.range} и осталось {monster.health} здоровья.")
 
-Class Sword(Weapon) #меч
+class Sword(Weapon): #меч
     def __init__(self, name, damage):
         self.name = name
         self.damage = damage
-    def attack(self, monster):
-        monster.health -= self.damage
+    def attack(self, monster,power):
+        monster.health -= self.damage*power
         if monster.health <= 0:
-            print(f"Победа! Монстр убит оружием {self.name}!") ")
+            print(f"Победа! {monster.name} убит оружием {self.name}!")
         else:
-            print(f"Боец наносит удар! Монстр получил {self.damage} урона оружием {self.name} и осталось {monster.health} здоровья.")
+            print(f"Боец наносит удар! {monster.name} получил {self.damage*power} урона оружием {self.name} и осталось {monster.health} здоровья.")
 
 
 # игрок
-Class Player()
+class Player:
     def __init__(self,fighter, weapon, monster):
         self.fighter = fighter
-        self.fihter.weapon = weapon
+        self.fighter.weapon = weapon
         self.monster = monster
+        print(f"Текущий боец: {self.fighter.name} с оружием {self.fighter.weapon.name} и монстр {self.monster.name} с уровнем здоровья {self.monster.health}!")
     def select_fighter(self,fighter):
         self.fighter = fighter
+    def select_monster(self,monster):
+        self.monster = monster
     def change_weapon(self, weapon):
         self.fighter.weapon = weapon
-        print(f"Боец {fighter.name} выбрал оружие {weapon.name}!")
+        print(f"Боец {self.fighter.name} выбрал оружие {weapon.name}!")
 
     def fight(self):
-        self.fighter.weapon.attack(self.monster)
+        self.fighter.weapon.attack(self.monster,self.fighter.power)
+
+
+gnome = Fighter("Гном",1)
+sword_master = Fighter("Мастер меча",10)
+archer = Fighter("Лучник",2)
+sword = Sword("Меч", 10)
+bow = Bow("Лук", 5, 5)
+long_bow = Bow("Длинный лук", 10, 10)
+troll = Monster("Тролль", 100)
+cyclopus = Monster("Циклоп", 50)
+gamer = Player(gnome, sword, cyclopus)
+gamer.fight()
+gamer.change_weapon(bow)
+gamer.fight()
+gamer.select_fighter(archer)
+gamer.change_weapon(long_bow)
+gamer.fight()
+gamer.fight()
+gamer.select_monster(troll)
+gamer.select_fighter(sword_master)
+gamer.change_weapon(sword)
+gamer.fight()
